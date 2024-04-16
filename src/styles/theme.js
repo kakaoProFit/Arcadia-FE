8c6e688b1df8b5742afaa397c71f8acaf9f457a0
Doromoro <khan1148943@gmail.com>
2024-04-16 23:50:10 +0900

    set dependency

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+'use client'
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    kakao: {
+      main: '#FFD700', // 노란색
+    },
+    naver: {
+      main: '#069729', // 초록색
+    },
+  },
+})
+
+export default theme
