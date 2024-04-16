8d3429e322794a7a7da4acfcdd05297c31a4d903
Doromoro <khan1148943@gmail.com>
2024-04-17 01:04:01 +0900

    add react-quill

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 import theme from '@/styles/theme';
+
 
 export const metadata = {
   title: 'Arcadia',
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
       <body>
         <AppRouterCacheProvider options={{ key: 'css' }}>
           <ThemeProvider theme={theme}>
+            <CssBaseline />
             {children}
           </ThemeProvider>
         </AppRouterCacheProvider>
