6cb5e3133de438b6c9138b0cdb004bdef22bb089
DongkwanKim00 <dkkim_it@naver.com>
2024-04-17 21:26:15 +0900

    Text Editor Component complete

--- src/app/[mypageSlug]/contact/page.js
+++ src/app/[mypageSlug]/contact/page.js
@@ -1,1 +1,27 @@
 // 매칭 신청서 페이지
+
+import TextEditor from "../../../components/textEditor/page.js";
+
+function Contact () {
+    return(
+        <>
+            <TextEditor />
+        </>
+    )
+}
+
+export async function getStaticPaths() { // 지금 mypageSlug에 데이터가 없으니까 더미 데이터로 테스트 하는 함수.
+    // 임시 값을 정의합니다.
+    const paths = [
+        { params: { mypageSlug: '1' } },
+        { params: { mypageSlug: '2' } },
+        // 필요한 만큼 임시 값을 추가할 수 있습니다.
+    ];
+
+    return {
+        paths,
+        fallback: false // fallback을 false로 설정하여 임시 값 이외의 경로로의 접근을 404 페이지로 리다이렉션합니다.
+    };
+}
+
+export default Contact;
---
+++ src/components/textEditor/page.js
@@ -0,0 +1,78 @@
+"use client"
+
+import 'react-quill/dist/quill.snow.css';
+import dynamic from 'next/dynamic'; // Dynamic Import. Quill Editor를 서버 측 모듈에 포함하지 않고, 런타임시에 모듈을 import할 수 있다.
+import styled from 'styled-components';
+
+const QuillWrapper = dynamic(() => import('react-quill'), {
+    ssr: false,
+    loading: () => <p>Loading ...</p>,
+});
+
+const modules = {
+    toolbar: [
+      [{ header: '1' }, { header: '2' }, { font: [] }],
+      [{ size: [] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [
+        { list: 'ordered' },
+        { list: 'bullet' },
+        { indent: '-1' },
+        { indent: '+1' },
+      ],
+    //   ['link', 'image', 'video'], // 요구사항에 없는 기능이라 일단 제외
+      ['clean'],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  }
+
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    // 'link', // 요구사항에 없는 기능이라 일단 제외.
+    // 'image',
+    // 'video',
+  ]
+
+  // 스타일드 컴포넌트를 이용하여 텍스트 에디터 스타일링
+    const StyledTextEditor = styled.div`
+    width: 70%; 
+    aspect-ratio: 1 / 1; /* 정사각형 비율 유지 */
+    margin: 0 auto; /* 가운데 정렬 */
+
+    /* Quill 에디터의 스타일을 수정 */
+    .ql-editor { /* 높이는 조절되지 않아서, quill 속성에서 직접 고쳐야 함 */
+        min-height: 400px; /* 에디터 내용의 최소 높이 */
+        font-size: 16px; /* 폰트 사이즈 변경 */
+    }
+    `;
+
+const TextEditor = () => {
+    return(
+        <>
+            <StyledTextEditor>
+                <QuillWrapper 
+                    modules={modules} 
+                    formats={formats} 
+                    theme="snow" 
+                    StyledTextEditor
+                />
+            </StyledTextEditor>
+        </>
+    );
+};
+
+export default TextEditor;
+
