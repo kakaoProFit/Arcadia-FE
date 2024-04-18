001647ddf10182c2667e8e82e8d74a9a699e3d90
DongkwanKim00 <dkkim_it@naver.com>
2024-04-18 12:51:29 +0900

    폴더명 수정 후 필요없는 부분 삭제

--- src/app/mypage/contact/page.js
+++ src/app/mypage/contact/page.js
@@ -10,17 +10,4 @@ function Contact () {
     )
 }
 
-export async function getStaticPaths() { // 지금 mypageSlug에 데이터가 없으니까 더미 데이터로 테스트 하는 함수.
-    // 임시 값을 정의
-    const paths = [
-        { params: { mypageSlug: '1' } },
-        { params: { mypageSlug: '2' } },
-    ];
-
-    return {
-        paths,
-        fallback: false // fallback을 false로 설정하여 임시 값 이외의 경로로의 접근을 404 페이지로 리다이렉션
-    };
-}
-
 export default Contact;
