2c015f7f1bf9774ddeaa414cc143016d95ab96fd
DongkwanKim00 <dkkim_it@naver.com>
2024-04-19 17:45:43 +0900

    contact/page.js SSR 사용

--- src/app/mypage/contact/page.js
+++ src/app/mypage/contact/page.js
@@ -1,11 +1,6 @@
 // 상담 신청서 조회 페이지
-"use client"
-
-import Button from '@mui/material/Button';
 import dynamic from 'next/dynamic';
-import { useState, useEffect } from 'react';
 import Stack from '@mui/material/Stack';
-import { useRouter } from 'next/navigation';
 
 // react-quill을 동적으로 임포트
 const ReadTextEditor = dynamic(() => import('@/components/textEditor/readTextEditor.js'), {
@@ -13,35 +8,6 @@ const ReadTextEditor = dynamic(() => import('@/components/textEditor/readTextEdi
 });
 
 function Contact() {
-    const [editorContent, setEditorContent] = useState("<p>test</p>\n<h1>test2</h2>"); // QuillEditor 내용 관리
-    const router = useRouter();
-
-    const handleGetConsultingSpecification = () => {
-        fetch('/mypage/${UserId}/Consulting', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(response => {
-                if (response.ok) {
-                    return response.json(); // JSON 형태의 응답  -> 이거 추후에 백엔드에서 넘어오는거 보고 수정 필요할듯
-                } else {
-                    console.error('실패');
-                }
-            })
-            .then(data => {
-                setEditorContent(data.content); // 받아온 내용을 QuillEditor에 설정
-            })
-            .catch(error => {
-                console.error('오류 발생', error);
-            });
-    }
-
-    useEffect(() => {
-        handleGetConsultingSpecification(); // 처음 렌더링 될 때 GET 요청을 보냄
-    }, []);
-
     return (
         <div>
             <div style={{marginLeft: "15%"}}>
@@ -53,13 +19,10 @@ function Contact() {
             <Stack direction="column" alignItems="center" spacing={2}>
                 <ReadTextEditor
                     theme="snow"
-                    value={editorContent}
                 />
-                <Button variant="contained" onClick={() => router.push("/matching")}>수정</Button>
             </Stack>
         </div>
     )
-
 };
 
 export default Contact;
--- src/components/textEditor/readTextEditor.js
+++ src/components/textEditor/readTextEditor.js
@@ -3,6 +3,9 @@
 import 'react-quill/dist/quill.snow.css';
 import styled from 'styled-components';
 import ReactQuill from 'react-quill';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Button from '@mui/material/Button';
 
 const modules = { // Quill의 동작과 기능을 사용자 정의
   toolbar: [
@@ -45,9 +48,42 @@ const StyledTextEditor = styled.div`
     min-height: 400px;
     font-size: 16px;
   }
+
+  > * {
+    margin-bottom: 20px; /* 버튼과 에디터 사이 간격 */
+  }
 `;
 
-const ReadTextEditor = ({ value }) => { // value: 사용자가 입력한 내용, onChage: value를 변경하기 위한 함수
+const ReadTextEditor = () => { // value: 사용자가 입력한 내용, onChage: value를 변경하기 위한 함수
+
+  const [editorContent, setEditorContent] = useState("<p>test</p>\n<h1>test2</h2>"); // QuillEditor 내용 관리
+  const router = useRouter();
+
+  useEffect(() => {
+    handleGetConsultingSpecification(); // 처음 렌더링 될 때 GET 요청을 보냄
+  }, []);
+
+  const handleGetConsultingSpecification = () => {
+    fetch('/mypage/${UserId}/Consulting', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json(); // JSON 형태의 응답  -> 이거 추후에 백엔드에서 넘어오는거 보고 수정 필요할듯
+        } else {
+          console.error('실패');
+        }
+      })
+      .then(data => {
+        setEditorContent(data.content); // 받아온 내용을 QuillEditor에 설정
+      })
+      .catch(error => {
+        console.error('오류 발생', error);
+      });
+  }
 
   return (
     <StyledTextEditor>
@@ -56,8 +92,9 @@ const ReadTextEditor = ({ value }) => { // value: 사용자가 입력한 내용,
         formats={formats}
         theme="snow"
         readOnly={true} // 편집 불가능
-        value={value} // 전달받은 value를 사용하여 텍스트를 표시
+        value={editorContent} // 백엔드 측에서 조회된 내용 표시
       />
+      <Button variant="contained" onClick={() => router.push("/matching")}>수정</Button> 
     </StyledTextEditor>
   );
 };
