ab295d547d3f0e16f805abfc1c09e700bc23122a
DongkwanKim00 <dkkim_it@naver.com>
2024-04-19 18:24:20 +0900

    get 요청방식 SSR에서 가능하도록 수정

--- src/app/mypage/contact/page.js
+++ src/app/mypage/contact/page.js
@@ -8,17 +8,43 @@ const ReadTextEditor = dynamic(() => import('@/components/textEditor/readTextEdi
 });
 
 function Contact() {
+
+    let response_data = "<h2>Hello World</h2>"
+
+    const handleGetConsultingSpecification = () => {
+        fetch('/mypage/${UserId}/Consulting', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json(); // JSON 형태의 응답  -> 이거 추후에 백엔드에서 넘어오는거 보고 수정 필요할듯
+                } else {
+                    console.error('실패');
+                }
+            })
+            .then(data => {
+                response_data = data
+            })
+            .catch(error => {
+                console.error('오류 발생', error);
+            });
+    }
+
     return (
         <div>
-            <div style={{marginLeft: "15%"}}>
+            <div style={{ marginLeft: "15%" }}>
                 <h2>마이 페이지</h2>
             </div>
-            <div style={{marginLeft: "15%"}}>
+            <div style={{ marginLeft: "15%" }}>
                 <p>탭 컨포넌트 들어갈 자리</p>
             </div>
             <Stack direction="column" alignItems="center" spacing={2}>
                 <ReadTextEditor
                     theme="snow"
+                    props = {response_data}
                 />
             </Stack>
         </div>
--- src/components/textEditor/readTextEditor.js
+++ src/components/textEditor/readTextEditor.js
@@ -3,9 +3,8 @@
 import 'react-quill/dist/quill.snow.css';
 import styled from 'styled-components';
 import ReactQuill from 'react-quill';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import Button from '@mui/material/Button';
+import { useRouter } from 'next/navigation';
 
 const modules = { // Quill의 동작과 기능을 사용자 정의
   toolbar: [
@@ -54,37 +53,8 @@ const StyledTextEditor = styled.div`
   }
 `;
 
-const ReadTextEditor = () => { // value: 사용자가 입력한 내용, onChage: value를 변경하기 위한 함수
-
-  const [editorContent, setEditorContent] = useState("<p>test</p>\n<h1>test2</h2>"); // QuillEditor 내용 관리
+const ReadTextEditor = ( props ) => { // value: 사용자가 입력한 내용, onChage: value를 변경하기 위한 함수
   const router = useRouter();
-
-  useEffect(() => {
-    handleGetConsultingSpecification(); // 처음 렌더링 될 때 GET 요청을 보냄
-  }, []);
-
-  const handleGetConsultingSpecification = () => {
-    fetch('/mypage/${UserId}/Consulting', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.json(); // JSON 형태의 응답  -> 이거 추후에 백엔드에서 넘어오는거 보고 수정 필요할듯
-        } else {
-          console.error('실패');
-        }
-      })
-      .then(data => {
-        setEditorContent(data.content); // 받아온 내용을 QuillEditor에 설정
-      })
-      .catch(error => {
-        console.error('오류 발생', error);
-      });
-  }
-
   return (
     <StyledTextEditor>
       <ReactQuill
@@ -92,7 +62,7 @@ const ReadTextEditor = () => { // value: 사용자가 입력한 내용, onChage:
         formats={formats}
         theme="snow"
         readOnly={true} // 편집 불가능
-        value={editorContent} // 백엔드 측에서 조회된 내용 표시
+        value={props.props} // 백엔드 측에서 조회된 내용 표시
       />
       <Button variant="contained" onClick={() => router.push("/matching")}>수정</Button> 
     </StyledTextEditor>
