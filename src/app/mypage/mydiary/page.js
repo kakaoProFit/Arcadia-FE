5bf4e45c24cdf8a13cc74f5de4fa296dd6f83098
Doromoro <khan1148943@gmail.com>
2024-04-24 19:47:09 +0900

    clean up

--- src/app/mypage/mydiary/page.js
+++ src/app/mypage/mydiary/page.js
@@ -3,10 +3,10 @@ import DiaryCard from '@/components/card/card'
 import DiaryPagination from '@/components/pagination/pagination'
 import { CARD_DUMMY_DATA } from '@/constants/CardDummy'
 
-export default function DiaryListPage({searchParams}) {
+function MyPageDiaryList() {
   // 현재 url 매개 변수를 받아옴.
-  const query = searchParams?.query || ''
-  const currentPage = Number(searchParams?.page) || 1
+  // const query = searchParams?.query || ''
+  // const currentPage = Number(searchParams?.page) || 1
   
   //더미 데이터
   // API 호출로 데이터 받아서 뿌릴 예정
@@ -16,18 +16,18 @@ export default function DiaryListPage({searchParams}) {
   let itemCount = 50
 
   return (
-    <>
+    <div>
       <Grid
         container
         rowSpacing={2}
         columnSpacing={2}
-        my={2}
         alignItems="center"
         justifyContent="center"
+        sx={{
+          mb: 5,
+        }}
       >
         {CARD.map((it, index) => (
-          // 기존에는 사이즈마다 다르게 했으나 pagination 구현하면서 일단은 개수 제한을 그냥 둠
-          // <Grid item xs={12} sm={6} md={3} lg={2} key={index}>
           <Grid item xs={2} key={index}>
             <DiaryCard
               key={index}
@@ -44,6 +44,8 @@ export default function DiaryListPage({searchParams}) {
         ))}
       </Grid>
       <DiaryPagination diaryCount={itemCount} />
-    </>
+    </div>
   )
 }
+
+export default MyPageDiaryList;
