2949374d242ae591b44bd46a4b3db7f9a023389c
KimuYounguWoo <dkp0803@gmail.com>
2024-05-09 20:10:56 +0900

    ✨ feat: card, post, tab component

--- src/app/mypage/page.js
+++ src/app/mypage/page.js
@@ -1,7 +1,25 @@
+import Tap from '@/components/Tap.jsx';
+import '@/styles/globals.css';
+import CardData from '@/components/CardData.jsx';
+import Card from '@/components/Card.jsx';
+import Post from '@/components/Post.jsx';
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    const data = CardData();
+    let response_data = {
+        // 게시글 테스트 데이터
+        diaryId: 1,
+        writer: '홍길동',
+        title: '일기란 무엇인가',
+        content: '<h2>오늘의 일기</h2>\n<p>오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기오늘의 일기날이 좋았다.</p>',
+        dirViews: '100', //조회수
+      }
+    return (
+    <div className="bg-white ">
+        <Tap/>
+        <div className="mx-10 font-tenada">
+            <Post theme="snow" props={response_data} />
+            {/* <Card data={data} count={6}/> */}
+        </div>
+    </div>)
 }
---
+++ src/components/Tap.jsx
@@ -0,0 +1,43 @@
+export default function Tap() {
+    const data = [
+        {
+            id: 1,
+            name: "자유",
+            href: "#free"
+        },
+        {
+            id: 2,
+            name: "정보",
+            href: "#info"
+        },
+        {
+            id: 3,
+            name: "질문",
+            href: "#question"
+        },
+        {
+            id: 4,
+            name: "일기",
+            href: "#diary"
+        }
+    ]
+    return (
+        <div className="bg-white mb-10 lg:px-6 py-2.5">
+            <div class="mx-auto font-tenada">
+                <div class="mx-10 border-b border-gray-200 dark:border-gray-700 mb-4">
+                    <ul class="flex flex-wrap -mb-px" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+                        {data.map((item) => (
+                            <li key={item.id} class="mr-2" role="presentation">
+                                <button
+                                    class="text-lg inline-block text-gray-500 hover:text-gray-600 hover:border-gray-300 rounded-t-lg py-4 px-4 font-medium text-center border-transparent border-b-2 dark:text-gray-400 dark:hover:text-gray-300"
+                                    id={item.id}>{item.name}
+                                    <a href= {item.href}></a>
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+        </div>
+    </div>
+    )
+}
