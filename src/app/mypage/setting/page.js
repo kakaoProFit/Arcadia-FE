613866a605ca885d957cc632bcb8eb12f19387b2
mango0422 <tom990422@gmail.com>
2024-05-28 23:21:20 +0900

    edit

--- src/app/mypage/setting/page.js
+++ src/app/mypage/setting/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import Modal from '@/components/Modal'
+import Modal from '@/components/modal'
 import RectangleSkeleton from '@/components/loading-skeleton/rectangle-skeleton'
 import { getProfileImage, getProfileInfo } from '@/services/profile-data'
 import { useState, useEffect } from 'react'
