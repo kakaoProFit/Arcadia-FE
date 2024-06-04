cf6e48be39c0e87107cb794f1de2b2d934bd6ac2
서용준 <syj0422@gachon.ac.kr>
2024-06-04 14:13:00 +0900

    edit next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode : false
+    reactStrictMode: false,
+    env: {
+        NEXT_PUBLIC_S3_accessKeyId: process.env.NEXT_PUBLIC_S3_accessKeyId,
+        NEXT_PUBLIC_S3_secretAccessKey: process.env.NEXT_PUBLIC_S3_secretAccessKey,
+        NEXT_PUBLIC_S3_region: process.env.NEXT_PUBLIC_S3_region,
+        NEXT_PUBLIC_S3_bucketName: process.env.NEXT_PUBLIC_S3_bucketName,
+    },
 };
 
 export default nextConfig;
