3216c18778fb8c34c7068fff2cbca25ffb3cd337
서용준 <syj0422@gachon.ac.kr>
2024-06-04 20:17:40 +0900

    edit config

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     env: {
-        NEXT_PUBLIC_S3_accessKeyId: process.env.NEXT_PUBLIC_S3_accessKeyId,
-        NEXT_PUBLIC_S3_secretAccessKey: process.env.NEXT_PUBLIC_S3_secretAccessKey,
-        NEXT_PUBLIC_S3_region: process.env.NEXT_PUBLIC_S3_region,
-        NEXT_PUBLIC_S3_bucketName: process.env.NEXT_PUBLIC_S3_bucketName,
+        NEXT_PUBLIC_S3_accessKeyId: process.env.S3_accessKeyId,
+        NEXT_PUBLIC_S3_secretAccessKey: process.env.S3_secretAccessKey,
+        NEXT_PUBLIC_S3_region: process.env.S3_region,
+        NEXT_PUBLIC_S3_bucketName: process.env.S3_bucketName,
     },
 };
 
